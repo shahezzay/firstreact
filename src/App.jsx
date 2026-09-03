5e50edfb676a294c6690d766bb38ba64c3c6fb3a
shahezzay <shahezzay@gmail.com>
2026-09-03 14:50:53 +0500

    First React Project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App(){
+  return(
+    <div>
+      <h1>This is my First React JS</h1>
+      {/*Ordered, Unordered, Tables, Embadding, Forms */}
+      <ul>Ali
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+      </ul>
+    </div>
+  )
+}
+export default App;
